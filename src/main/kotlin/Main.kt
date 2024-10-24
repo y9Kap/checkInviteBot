f1b9bea60535524391b5f1fa225fbd80b0c20107
y9Kap <wotblitz6allima4ok@gmail.com>
2024-10-24 16:45:38 +0300

    add logs and data info

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ import dev.inmo.tgbotapi.extensions.api.send.*
 import dev.inmo.tgbotapi.extensions.behaviour_builder.*
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.*
 import dev.inmo.tgbotapi.extensions.utils.*
+import dev.inmo.tgbotapi.extensions.utils.extensions.raw.from
 import dev.inmo.tgbotapi.extensions.utils.types.buttons.dataButton
 import dev.inmo.tgbotapi.types.ChatId
 import dev.inmo.tgbotapi.types.MessageId
@@ -13,6 +14,7 @@ import dev.inmo.tgbotapi.types.RawChatId
 import dev.inmo.tgbotapi.types.buttons.InlineKeyboardMarkup
 import dev.inmo.tgbotapi.utils.matrix
 import dev.inmo.tgbotapi.utils.row
+import kotlinx.coroutines.launch
 
 suspend fun main() {
     val token: String = System.getenv("BOT_TOKEN")
@@ -21,20 +23,37 @@ suspend fun main() {
     val bot = telegramBot(token)
 
     bot.buildBehaviourWithLongPolling {
+        launch {
+            allUpdatesFlow.collect { update ->
+                println(update)
+            }
+        }
+        onCommand("start") {
+            if (it.from?.id?.chatId?.long == adminChatIdLong) {
+                bot.sendMessage(
+                    chatId = it.chat.id,
+                    text = "Добро пожаловать в бот по отслеживанию запросов на вступление."
+                )
+            }
+        }
         onChatJoinRequest {
             val member = it.user
-            val messageId = bot.sendMessage(
+            val message = bot.sendMessage(
                 chatId = adminChatId,
                 text = "User ${member.firstName} with username ${member.username} and premium(${member.possiblyPremiumChatOrNull()?.isPremium}) want to join to ${it.chat.usernameChatOrNull()?.username?.username}"
-            ).messageId
+            )
             bot.editMessageText(
                 adminChatId,
-                messageId,
+                message.messageId,
                 text = "User ${member.firstName} with username ${member.username} and premium(${member.possiblyPremiumChatOrNull()?.isPremium}) want to join to ${it.chat.usernameChatOrNull()?.username?.username}",
                 replyMarkup = InlineKeyboardMarkup(
                     keyboard = matrix {
                         row {
-                            dataButton("Просмотрел, удалить уведомление", "deleteNotifivation+${messageId.long}")
+                            dataButton(
+                                "Просмотрел, удалить уведомление", "deleteNotifivation+" +
+                                        "${message.messageId.long}" +
+                                        "+${message.chat.id.chatId.long}"
+                            )
                         }
                     }
                 )
@@ -44,8 +63,10 @@ suspend fun main() {
         onDataCallbackQuery {
             val data = it.data
             if (data.startsWith("deleteNotifivation+")) {
-                val messageId = data.split("+")[1].toLong()
-                bot.deleteMessage(adminChatId, MessageId(messageId))
+                val parts = data.split("+")
+                val messageId = parts[1].toLong()
+                val chatId = parts[2].toLong()
+                bot.deleteMessage(ChatId(RawChatId(chatId)), MessageId(messageId))
             }
         }
     }.join()
