db9425cfaaed11812e048017c69271275bd40eb0
y9Kap <wotblitz6allima4ok@gmail.com>
2024-10-24 15:21:37 +0300

    add application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,22 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    application
 }
+group = "org.example"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation ("dev.inmo:tgbotapi:18.2.2")
+    implementation("dev.inmo:tgbotapi:18.2.2")
 }
 
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass = "org.example.MainKt"
 }
